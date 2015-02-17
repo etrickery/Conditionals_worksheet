2792ebd99073738d9ebbd3f74aa708da8f798227
Etrickery <ericspfister@gmail.com>
2015-02-16 20:30:37 -0500

    variables
    group one variables

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//Eric Pfister
+//February 16th, 2015
+//SDI 1502
+//Section 01
+//Activity: Conditionals Worksheet
+
+//Group 1: Expressions with Conditionals
+//Last chance for gas
+var mpg;
+var left;
+var capacity;
+var distance = 200;
+var stopForFuel = true;
+
+
+
+
+
+
+//Group 2: Multiple Results
+//
+
+
+
+//Group 3: Multiple Conditions
+//
+
+
+
+
+
